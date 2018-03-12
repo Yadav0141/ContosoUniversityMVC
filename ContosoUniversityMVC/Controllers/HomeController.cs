d54e1413871bcbbd90dc79489a82aa45fb31bf67
abhishek kumar <abhishekyadav@benchmarkitsolutions.com>
2018-03-12 20:10:35 +0530

    sorting and filtering

--- ContosoUniversityMVC/Controllers/HomeController.cs
+++ ContosoUniversityMVC/Controllers/HomeController.cs
@@ -5,21 +5,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ContosoUniversityMVC.Models;
+using ContosoUniversityMVC.Models.SchoolViewModels;
+using ContosoUniversityMVC.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContosoUniversityMVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SchoolContext _context;
+        public HomeController(SchoolContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public async Task<ActionResult> About()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            IQueryable<EnrollmentDateGroup> data =
+            from student in _context.Students
+            group student by student.EnrollmentDate into dateGroup
+            select new EnrollmentDateGroup()
+            {
+                EnrollmentDate = dateGroup.Key,
+                StudentCount = dateGroup.Count()
+            };
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         public IActionResult Contact()
